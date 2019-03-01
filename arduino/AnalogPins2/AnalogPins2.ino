a20bdd3146cc2b243351ef9b081ae88bb4df8046
Mark IJzerman <markijzerman@gmail.com>
2019-03-01 14:53:24 +0100

    first commit

---
+++ arduino/AnalogPins2/AnalogPins2.ino
@@ -0,0 +1,47 @@
+#define loops 10
+static const uint8_t analog_pins[] = {A0,A1,A2,A3,A4,A5};
+int delayTime = 2;
+int thisPin = 0;
+int checkPin = 0;
+int sensorValue = 0;
+int maxPin = 4;
+
+void setup() {
+  Serial.begin(115200);
+  
+  
+}
+
+void loop() { 
+  if(thisPin > maxPin) {
+    thisPin = 0;
+  }
+  
+  pinMode(analog_pins[thisPin], INPUT_PULLUP);
+
+  for (int x = thisPin; x < maxPin; x++) {
+    for (int k = 0; k < loops; k++) {
+      
+      checkPin = x + 1;
+      sensorValue = analogRead(analog_pins[checkPin]);
+
+      if(sensorValue > 350) {
+        Serial.print("CONNECTION ");
+        Serial.print(thisPin);
+        Serial.print(" & ");
+        Serial.println(checkPin);
+    }
+    
+      delay(delayTime);        // delay in between reads for stability
+    }
+
+  }
+  
+  digitalWrite(analog_pins[thisPin], 0) ;
+  
+  for (int k = 0; k < loops; k++) {
+    delay(delayTime);        // delay in between reads for stability
+  }
+
+  thisPin++;
+}
